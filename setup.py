0978b0d1ea57bb9d1ba5dd5385675ff08536f3cd
ToniWestbrook <anthony.westbrook@unh.edu>
2020-06-08 17:30:23 -0400

    Release/0.9.1

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='RepeatFS',
+    version='0.9.1',
+    author='Anthony Westbrook',
+    author_email='anthony.westbrook@unh.edu',
+    packages=setuptools.find_packages(),
+    scripts=['repeatfs/repeatfs.py'],
+    url='http://github.com/ToniWestbrook/repeatfs',
+    license='LICENSE',
+    description='File system providing reproducibility through provenance and automation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Langauge :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    install_requires=[
+        "fusepy >= 3.0.1",
+        "python-daemon >= 2.2.0",
+        "pygraphviz >= 1.3.1",
+    ],
+)
